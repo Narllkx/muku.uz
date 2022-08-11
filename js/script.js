1c0e1cbbc7b0a9a38e61404f11afe787eea9d9cc
Narllkx <87603907+Narllkx@users.noreply.github.com>
2022-08-11 12:25:52 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.header-burger').click(function(event) {
+        $('.header-burger,.header-menu').toggleClass('active');
+        $('body').toggleClass('lock');
+        $('.website-menu').toggleClass('active')
+    });
+});
